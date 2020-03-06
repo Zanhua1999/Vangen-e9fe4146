07677b7a12247ed3024dcf5bccc73aa8f859aaef
Zanhua1999 <skyair886@gmail.com>
2020-03-06 10:16:37 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,71 @@
+function gooiButton() {
+    try{
+    bal.goi();
+    }
+catch(err){
+    alert("Fout, de bal is niet in de lucht");
+}
+}
+
+function vangButton() {
+    try{
+    bal.vang();
+}
+catch{
+    console.log("Fout2")
+}
+}
+
+function resetButton() {
+    try{
+    bal.reset();
+}
+catch{
+    console.log("Fout3")
+}
+}
+
+
+var bal = {
+    canvasBal: constructBal(),
+    balPositie: "links",
+
+    gooi: function () {
+        if (this.balPositie !== "links") {
+            throw Error("bal in verkeerde positie")
+        }
+        this.draw(300, 50);
+        this.balPositie = "midden";
+    },
+
+    vang: function () {
+        if (this.balPositie !== "midden") {
+            throw Error("bal in verkeerde positie")
+        }
+        this.draw(500, 250);
+        this.balPositie = "rechts";
+    },
+
+    reset: function () {
+        this.draw(100, 250);
+        this.balPositie = "links";
+    },
+
+    draw: function (x, y) {
+        this.canvasBal.clearRect(0, 0, 600, 300);
+        this.canvasBal.beginPath();
+        this.canvasBal.arc(x, y, 50, 0, 2 * Math.PI);
+        this.canvasBal.closePath();
+        this.canvasBal.fill();
+    }
+}
+
+function constructBal() {
+    let bal = document.getElementById("bal").getContext("2d");
+    bal.fillStyle = "red";
+    bal.beginPath();
+    bal.arc(100, 250, 50, 0, 2 * Math.PI);
+    bal.closePath();
+    bal.fill();
+    return bal
+}
